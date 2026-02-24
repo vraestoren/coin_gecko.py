579e6a0b52ff8213df9082b3c90e05f2730b05bf
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 14:50:14 +0300

    Update coin_gecko.py

--- src/coin_gecko.py
+++ src/coin_gecko.py
@@ -1,17 +1,16 @@
-from requests import get
+from requests import Session
 
 class CoinGecko:
 	def __init__(self) -> None:
 		self.api = "https://api.coingecko.com"
-		self.headers = {
+		self.session = Session()
+		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
 			"accept": "application/json"
 		}
 	
 	def ping(self) -> dict:
-		return get(
-			f"{self.api}/ping",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/ping").json()
 	
 	def get_price(
 			self,
@@ -20,9 +19,8 @@ class CoinGecko:
 			include_market_cap: bool = False,
 			include_24hr_vol: bool = False,
 			include_last_updated_at: bool = False) -> dict:
-		return get(
-			f"{self.api}/simple/price?ids={coins_ids}&vs_currencies={vs_currencies}&include_market_cap={include_market_cap}&include_24hr_vol={include_24hr_vol}&include_last_updated_at={include_last_updated_at}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/simple/price?ids={coins_ids}&vs_currencies={vs_currencies}&include_market_cap={include_market_cap}&include_24hr_vol={include_24hr_vol}&include_last_updated_at={include_last_updated_at}").json()
 	
 	def get_token_price(
 			self,
@@ -32,19 +30,16 @@ class CoinGecko:
 			include_market_cap: bool = False,
 			include_24hr_vol: bool = False,
 			include_last_updated_at: bool = False) -> dict:
-		return get(
-			f"{self.api}/simple/token_price/{platform_id}?vs_currencies={vs_currencies}&include_market_cap={include_market_cap}&include_24hr_vol={include_24hr_vol}&include_last_updated_at={include_last_updated_at}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/simple/token_price/{platform_id}?vs_currencies={vs_currencies}&include_market_cap={include_market_cap}&include_24hr_vol={include_24hr_vol}&include_last_updated_at={include_last_updated_at}").json()
 	
 	def get_supported_vs_currencies(self) -> dict:
-		return get(
-			f"{self.api}/simple/supported_vs_currencies",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/simple/supported_vs_currencies").json()
 	
 	def get_coins_list(self, include_platform: bool = False) -> dict:
-		return get(
-			f"{self.api}/coins/list?include_platform={include_platform}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/list?include_platform={include_platform}").json()
 	
 	def get_markets(
 			self,
@@ -56,15 +51,18 @@ class CoinGecko:
 			page: int = 1,
 			sparkline: bool = False,
 			price_change_percentage: str = None) -> dict:
-		url = f"{self.api}/coins_markets?vs_currency={vs_currency}&order={order}&per_page={per_page}&page={page}&sparkline={sparkline}"
-		if coin_ids:
-			url += f"&ids={coin_ids}"
-		if category:
-			url += f"&category={category}"
-		if price_change_percentage:
-			url += f"&price_change_percentage={price_change_percentage}"
-		return get(
-			url, headers=self.headers).json()
+		params = {
+			"ids": coin_ids,
+			"category": category,
+			"price_change_percentage": price_change_percentage,
+
+		}
+		filtered_params = {
+			key: value for key, value in params.items() if value is not None
+		}
+		return self.session.get(
+			f"{self.api}/coins_markets?vs_currency={vs_currency}&order={order}&per_page={per_page}&page={page}&sparkline={sparkline}",
+			params=filtered_params).json()
 	
 	def get_coin_current_data(
 			self,
@@ -75,9 +73,8 @@ class CoinGecko:
 			community_data: bool = True,
 			developer_data: bool = True,
 			sparkline: bool = False) -> dict:
-		return get(
-			f"{self.api}/coins/{coin_id}?localization={localization}&tickers={tickers}&market_data={market_data}&community_data={community_data}&developer_data={developer_data}&sparkline={sparkline}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}?localization={localization}&tickers={tickers}&market_data={market_data}&community_data={community_data}&developer_data={developer_data}&sparkline={sparkline}").json()
 	
 	def get_coin_tickers(
 			self,
@@ -87,26 +84,26 @@ class CoinGecko:
 			page: int = 1,
 			order: str = None,
 			depth: str = None) -> dict:
-		url = f"{self.api}/coins/{coin_id}/tickers?page={page}"
-		if exchange_ids:
-			url += f"&exchange_ids={exchange_ids}"
-		if include_exchange_logo:
-			url += f"&include_exchange_logo={include_exchange_logo}"
-		if order:
-			url += f"&order={order}"
-		if depth:
-			url += f"&depth={depth}"
-		return get(
-			url, headers=self.headers).json()
+		params = {
+			"exchange_ids": exchange_ids,
+			"include_exchange_logo": include_exchange_logo,
+			"order": order,
+			"depth": depth
+		}
+		filtered_params = {
+			key: value for key, value in params.items() if value is not None
+		}
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}/tickers?page={page}",
+			params=filtered_params).json()
 	
 	def get_coin_historical_data(
 			self,
 			coin_id: str,
 			date: str, 
 			localization: bool = True) -> dict:
-		return get(
-			f"{self.api}/coins/{coin_id}/history?date={date}&localization={localization}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}/history?date={date}&localization={localization}").json()
 	
 	def get_coin_market_chart(
 			self,
@@ -114,9 +111,8 @@ class CoinGecko:
 			vs_currency: str,
 			days: str,
 			interval: str = "daily") -> dict:
-		return get(
-			f"{self.api}/coins/{coin_id}/market_chart?vs_currency={vs_currency}&days={days}&interval={interval}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}/market_chart?vs_currency={vs_currency}&days={days}&interval={interval}").json()
 
 	def get_coin_market_chart_range(
 			self,
@@ -124,17 +120,15 @@ class CoinGecko:
 			vs_currency: str,
 			from_date: int,
 			to_date: int) -> dict:
-		return get(
-			f"{self.api}/coins/{coin_id}/market_chart/range?vs_currency={vs_currency}&from={from_date}&to={to_date}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}/market_chart/range?vs_currency={vs_currency}&from={from_date}&to={to_date}").json()
 	
 	def get_coin_info_by_contract_address(
 			self,
 			platform_id: str,
 			contract_address: str) -> dict:
-		return get(
-			f"{self.api}/coins/{platform_id}/contract/{contract_address}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{platform_id}/contract/{contract_address}").json()
 	
 	def get_contract_address_market_chart(
 			self,
@@ -142,9 +136,8 @@ class CoinGecko:
 			contract_address: str,
 			vs_currency: str,
 			days: str) -> dict:
-		return get(
-			f"{self.api}/coins/{platform_id}/contract/{contract_address}/market_chart?vs_currency={vs_currency}&days={days}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{platform_id}/contract/{contract_address}/market_chart?vs_currency={vs_currency}&days={days}").json()
 	
 	def get_contract_address_market_chart_range(
 			self,
@@ -153,53 +146,42 @@ class CoinGecko:
 			vs_currency: str,
 			from_date: int,
 			to_date: int) -> dict:
-		return get(
-			f"{self.api}/coins/{platform_id}/contract/{contract_address}/market_chart/range?vs_currency={vs_currency}&from={from_date}&to={to_date}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{platform_id}/contract/{contract_address}/market_chart/range?vs_currency={vs_currency}&from={from_date}&to={to_date}").json()
 	
 	def get_coin_ohlc(
 			self,
 			coin_id: str,
 			vs_currency: str,
 			days: str) -> dict:
-		return get(
-			f"{self.api}/coins/{coin_id}/ohlc?vs_currency={vs_currency}&days={days}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/{coin_id}/ohlc?vs_currency={vs_currency}&days={days}").json()
 	
 	def get_asset_platforms(self) -> dict:
-		return get(
-			f"{self.api}/asset_platforms",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/asset_platforms").json()
 	
 	def get_all_categories(self) -> dict:
-		return get(
-			f"{self.api}/coins/categories/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/coins/categories/list").json()
 	
 	def get_all_categories_with_market_data(self, order: str = None) -> dict:
-		url = f"{self.api}/coins/categories"
-		if order:
-			url += f"?order={order}"
-		return get(
-			url, headers=self.headers).json()
+		params = {"order": order} if order else {}
+		return self.session.get(
+			f"{self.api}/coins/categories", params=params).json()
 	
 	def get_all_exchanges(
 			self,
 			per_page: int = 100,
 			page: int = 1) -> dict:
-		return get(
-			f"{self.api}/exchanges?per_page={per_page}&page={page}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/exchanges?per_page={per_page}&page={page}").json()
 	
 	def get_exchanges_with_market_data(self) -> dict:
-		return get(
-			f"{self.api}/exchanges/list",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/exchanges/list").json()
 	
 	def get_exchange(self, exchange_id: str) -> dict:
-		return get(
-			f"{self.api}/exchanges/{exchange_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/exchanges/{exchange_id}").json()
 	
 	def get_exchange_tickers(
 			self,
@@ -209,101 +191,85 @@ class CoinGecko:
 			page: int = 1,
 			order: str = None,
 			depth: str = None) -> dict:
-		url = f"{self.api}/exchanges/{exchange_id}/tickers?page={page}"
-		if coin_ids:
-			url += f"&coin_ids={coin_ids}"
-		if include_exchange_logo:
-			url += f"&include_exchange_logo={include_exchange_logo}"
-		if order:
-			url += f"&order={order}"
-		if depth:
-			url += f"&depth={depth}"
-		return get(
-			url, headers=self.headers).json()
+		params = {
+			"coin_ids": coin_ids,
+			"include_exchange_logo": include_exchange_logo,
+			"order": order,
+			"depth": depth
+		}
+		filtered_params = {
+			key: value for key, value in params.items() if value is not None
+		}
+		return self.session.get(
+			f"{self.api}/exchanges/{exchange_id}/tickers?page={page}",
+			params=filtered_params).json()
 	
 	def get_all_market_indexes(
 			self,
 			per_page: int = 100,
 			page: int = 1) -> dict:
-		return get(
-			f"{self.api}/indexes?per_page={per_page}&page={page}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/indexes?per_page={per_page}&page={page}").json()
 	
 	def get_market_index(
 			self,
 			market_id: str,
 			index_id: str) -> dict:
-		return get(
-			f"{self.api}/indexes/{market_id}/{index_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/indexes/{market_id}/{index_id}").json()
 	
 	def get_indexes_with_market_data(self) -> dict:
-		return get(
-			f"{self.api}/indexes/list",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/indexes/list").json()
 	
 	def get_all_derivative_tickers(
 			self,
 			include_tickers: str = "unexpired") -> dict:
-		return get(
-			f"{self.api}/derivatives?include_tickers={include_tickers}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/derivatives?include_tickers={include_tickers}").json()
 	
 	def get_all_derivative_exchanges(
 			self,
 			order: str = None,
 			per_page: int = 100,
 			page: int = 1) -> dict:
-		url = f"{self.api}/derivatives/exchanges?per_page={per_page}&page={page}",
-		if order:
-			url += f"&order={order}"
-		return get(
-			url, headers=self.headers).json()
+		params = {"order": order} if order else {}
+		return self.session.get(
+			f"{self.api}/derivatives/exchanges?per_page={per_page}&page={page}",
+			params=params).json()
 	
 	def get_derivative_exchange_data(
 			self,
 			exchange_id: str,
 			include_tickers: str = "all") -> dict:
-		return get(
-			f"{self.api}/derivatives/exchanges/{exchange_id}?include_tickers={include_tickers}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/derivatives/exchanges/{exchange_id}?include_tickers={include_tickers}").json()
 	
 	def get_derivative_exchanges_list(self) -> dict:
-		return get(
-			f"{self.api}/derivative/exchanges/list",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/derivative/exchanges/list").json()
 	
 	def get_volume_chart_data(self, exchange_id: str, days: int) -> dict:
-		return get(
-			f"{self.api}/exchanges/{exchange_id}/volume_chart?days={days}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/exchanges/{exchange_id}/volume_chart?days={days}").json()
 	
 	def get_exchange_rates(self) -> dict:
-		return get(
-			f"{self.api}/exchange_rates",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/exchange_rates").json()
 	
 	def search(self, query: str) -> dict:
-		return get(
-			f"{self.api}/search?query={query}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/search?query={query}").json()
 	
 	def get_trending_search(self) -> dict:
-		return get(
-			f"{self.api}/search/trending",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/search/trending").json()
 	
 	def get_global_crypto_data(self) -> dict:
-		return get(
-			f"{self.api}/global",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/global").json()
 	
 	def get_global_crypto_defi_data(self) -> dict:
-		return get(
-			f"{self.api}/global/decentralized_finance_defi",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/global/decentralized_finance_defi").json()
 	
 	def get_public_companies_data(self, coin_id: str) -> dict:
-		return get(
-			f"{self.api}/companies/public_treasury/{coin_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/companies/public_treasury/{coin_id}").json()
